d0a3bb03ef20a2b58d2f9bed097fd494cc7d058e
AlphaDelta38 <kiril.morozov.01@gmail.com>
2024-10-30 10:15:52 +0300

    change lab6

--- lab6.sql
+++ lab6.sql
@@ -1,1 +1,4 @@
-SELECT cities.name, region FROM cities,regions WHERE regions.name = "Nord" AND cities.region = regions.uuid  AND population > 350000;
+SELECT cities.name, regions.name FROM cities,regions WHERE population > 350000;
+
+
+SELECT cities.name, region FROM cities,regions WHERE regions.name = "Nord" AND cities.region = regions.uuid;
